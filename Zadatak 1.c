ab4067c212c8c44186127069e9edf848918723cd
Anchi456 <95848089+Anchi456@users.noreply.github.com>
2022-03-10 09:07:31 +0100

    Add files via upload

---
+++ Zadatak 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+ int broj;
+ printf("Unesi broj: ");
+ scanf("%d",&broj);
+ if(broj%2==0)
+    printf("Broj je paran");
+ if(broj%2==1)
+    printf("Broj je neparan");3
+
+
+
+
+}
+
+
+
+
