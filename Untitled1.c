09ac91d4ff5c2fa99e9118c84ef1dbe61b935539
Anchi456 <95848089+Anchi456@users.noreply.github.com>
2022-03-10 10:38:47 +0100

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+ char x;
+ float a;
+ printf("Unesi o ili p");
+ scanf("%c"&x);
+ printf("unesi duljinu stranice");
+ scanf("%f",&a)
+switch(x){
+
+
+
+}
+
+
+
+
+
+}
 
